f01f2a79313e0ef4f16030db1a542f6ead4eaeb5
LeviDLima1 <163362987+LeviDLima1@users.noreply.github.com>
2024-09-02 11:30:03 -0300

    Update index.js

--- src/Js/index.js
+++ src/Js/index.js
@@ -1,5 +1,5 @@
 const voltar_bnt = document.querySelector('#voltar')
 
 voltar_bnt.addEventListener('click', () => {
-    window.location.href = 'Digital-College/index.html'
+    window.location.href = 'index.html'
 })
